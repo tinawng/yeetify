70aa843de0f77f4f406479ec06c8fd6aa04f7220
Tina Wang <tina@tina.cafe>
2021-01-24 16:18:41 +0100

    ⚡️ Add compression & optimize code

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 require('dotenv').config()
-var http2 = require('http2');
-var path = require('path');
-var fs = require('fs');
-var CachedFs = require('cachedfs'), cfs = new CachedFs();
+const http2 = require('http2');
+const path = require('path');
+const fs = require('fs');
+const CachedFs = require('cachedfs'), cfs = new CachedFs();
+
+const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.woff2': 'application/font-woff2',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.wasm': 'application/wasm'
+};
 
 http2.createSecureServer({
     key: fs.readFileSync(process.env.CERT_PATH + 'privkey.pem'),
@@ -11,34 +30,24 @@ http2.createSecureServer({
 }, function (request, response) {
     console.log('request ', request.url);
 
-    var filePath = request.url;
-    if (filePath == '/') {
-        filePath = '/index.html';
+    var filePath = request.url == '/' ? '/index.html' : request.url;
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    var encoding = ''
+    if (['.html', '.js', '.css'].includes(extname)) {
+        if (request.headers['accept-encoding'].includes('br')) {
+            encoding = '.br'
+            response.setHeader('Content-Encoding', 'br')
+        }
+        else {
+            encoding = '.gz'
+            response.setHeader('Content-Encoding', 'gzip')
+        }
     }
 
-    var extname = String(path.extname(filePath)).toLowerCase();
-    var mimeTypes = {
-        '.html': 'text/html',
-        '.js': 'text/javascript',
-        '.css': 'text/css',
-        '.json': 'application/json',
-        '.png': 'image/png',
-        '.jpg': 'image/jpg',
-        '.gif': 'image/gif',
-        '.svg': 'image/svg+xml',
-        '.wav': 'audio/wav',
-        '.mp4': 'video/mp4',
-        '.woff': 'application/font-woff',
-        '.woff2': 'application/font-woff2',
-        '.ttf': 'application/font-ttf',
-        '.eot': 'application/vnd.ms-fontobject',
-        '.otf': 'application/font-otf',
-        '.wasm': 'application/wasm'
-    };
-
-    var contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    cfs.readFile('./dist' + filePath, function (error, content) {
+    cfs.readFile('./dist' + filePath + encoding, function (error, content) {
         if (error) {
             if (error.code == 'ENOENT') {
                 cfs.readFile('./404.html', function (error, content) {
