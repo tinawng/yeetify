6bea1b4228afd8f04e2212151d5c048f596f1a8e
Tina Wang <fabzidentiste@gmail.com>
2021-05-03 14:18:03 +0200

    ⚡️ Forgot to use cache

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ http2.createSecureServer({
                 });
             }
             else if (error.code == 'EISDIR') {
-                fs.readFile('./dist' + filePath + '/index.html', function (error, content) {
+                cfs.readFile('./dist' + filePath + '/index.html', function (error, content) {
                     response.writeHead(200, { 'Content-Type': 'text/html' });
                     response.end(content, 'utf-8');
                 });
