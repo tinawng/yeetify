7672df5c2f42b42b5585a82e3d7a53855ba1324a
Tina Wang <fabzidentiste@gmail.com>
2023-03-10 14:31:33 +0100

    ⚡ Lower log worker pool count

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (log_request) {
 
     // 🐜 Create workers pool
     log_worker_pool = new StaticPool({
-        size: 3,
+        size: 2,
         task: async ({ log_api_token, payload }) => {
             await fetch(process.env.LOG_API_URL + "/api/collections/records/records", {
                 headers: { "Referer": process.env.NODE_NAME, 'Content-Type': 'application/json', 'Authorization': log_api_token },
